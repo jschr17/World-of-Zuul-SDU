d54d792420d56069a6800772d4cf0929f888e9c3
Stewil <32218463+Stewil@users.noreply.github.com>
2017-09-29 13:36:29 +0200

    Lavet en Start.Java, + ændrede navne på pakker i alle filer, så spillet virker

---
+++ Gruppe 6 Semester Projekt/src/Zuul_Framework/Start.java
@@ -0,0 +1,15 @@
+
+package Zuul_Framework;
+
+/**
+ *
+ * @author Wilde
+ */
+public class Start {
+    public static void main(String[] args) {
+        Game game1 = new Game();
+        game1.play();
+    }
+    
+    
+}
