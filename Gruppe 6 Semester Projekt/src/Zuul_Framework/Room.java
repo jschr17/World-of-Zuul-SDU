8efc370ff97e715bdf95ce2aa57cc7a0047f1b53
Rasje17 <32257308+Rasje17@users.noreply.github.com>
2017-10-08 11:13:54 +0200

    engelske kommentare

--- Gruppe 6 Semester Projekt/src/Zuul_Framework/Room.java
+++ Gruppe 6 Semester Projekt/src/Zuul_Framework/Room.java
@@ -9,32 +9,33 @@ import java.util.Iterator;
  * @author  Michael Kolling and David J. Barnes
  * @version 2006.03.30
  */
+// this class is used for crating room objects for the player to transition between
 public class Room 
 {
-    private String description;
-    private HashMap<String, Room> exits;
-
+    private String description;             //a sting that will carry the description of the room
+    private HashMap<String, Room> exits;    // HashMap that carrys each exit from the room and which room it leads to
+    //constructor that sets the rooms description
     public Room(String description) 
     {
         this.description = description;
-        exits = new HashMap<String, Room>();
+        exits = new HashMap<String, Room>();    // a new exit HashMap is crated for each instance of room
     }
-
+    //method for setting the exits of a room with a direction (key) and a neighbor room object
     public void setExit(String direction, Room neighbor) 
     {
         exits.put(direction, neighbor);
     }
-
+    //method for returning a rooms description
     public String getShortDescription()
     {
         return description;
     }
-
+    // method for getting a rooms description and which exits it has.
     public String getLongDescription()
     {
         return "You are " + description + ".\n" + getExitString();
     }
-
+        //private method that is used in here to descripe the exits based on the rooms exit HashMap 
 	private String getExitString()
     {
         String returnString = "There are exits to the ";
@@ -44,7 +45,7 @@ public class Room
         }
         return returnString;
     }
-
+        //method for getting the exits from a room
     public Room getExit(String direction) 
     {
         return exits.get(direction);
