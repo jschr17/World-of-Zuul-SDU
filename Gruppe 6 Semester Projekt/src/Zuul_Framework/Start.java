4ea94edf6503b24076ce64c5538e43e9d53df7f2
Stewil <32218463+Stewil@users.noreply.github.com>
2017-10-09 10:12:16 +0200

    Update Start.java
    rettede et par tastefejl

--- Gruppe 6 Semester Projekt/src/Zuul_Framework/Start.java
+++ Gruppe 6 Semester Projekt/src/Zuul_Framework/Start.java
@@ -5,7 +5,7 @@ package Zuul_Framework;
  *
  * @author Wilde
  */
-// the main Class of the game that initialices the Game() class and youses the play method on it
+// the main Class of the game that initialises the Game() class and uses the play method on it
 public class Start {
     public static void main(String[] args) {
         Game game1 = new Game();
