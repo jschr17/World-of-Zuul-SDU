3137235f3272e0afb0802d58ce321900493fd4f3
Stewil <32218463+Stewil@users.noreply.github.com>
2017-10-13 12:02:10 +0200

    changed name of class from Table to Immovable, as it is now for creating universal immovable objects.

---
+++ Gruppe 6 Semester Projekt/src/Zuul_Framework/Immovable.java
@@ -0,0 +1,52 @@
+package Zuul_Framework;
+
+/**
+ *
+ * @author Wilde
+ */
+public class Immovable extends Destructables{
+    private String name, description, useDescription;
+    private boolean destructible;
+
+    public Immovable(String name, String description, String useDescription, boolean destructible){
+        this.name = name;
+        this.description = description;
+        this.useDescription = useDescription;
+        this.destructible = destructible;
+        
+         
+    }
+        
+    //public method to break the table in the armoury
+    public void breakTable(){
+        //check to see if table is destructible before running the rest
+        if (this.destructible == true) {
+            System.out.println("You break the leg off the table \n a bunch of"
+                    + "notes fall on the floor.");
+            //changes the useDescription of the table.
+            this.useDescription = "There is a leg missing from the table";
+        /*    
+        **event when breaking table
+        **remove notes from table
+        **pickup table leg
+        */               
+        }
+        else
+            System.out.println("You can't break this.");
+        
+    }
+
+        
+    public String getName(){
+        return name;
+    }
+        
+    @Override /*Why does netbeans say this needs to be here?*/
+    public String getDescription(){
+        return description;
+    }
+        
+    public String getUseDescription(){
+        return useDescription;
+    }
+}
