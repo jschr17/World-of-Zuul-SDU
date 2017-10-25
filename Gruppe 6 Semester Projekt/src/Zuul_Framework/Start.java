b9b121d1ec703af44316be0a381cb67102de1656
Rasmus <Rasmus@DESKTOP-IO15SRG>
2017-10-25 13:23:03 +0200

    added a try-catch to Start to try and catch the ioexeption

--- Gruppe 6 Semester Projekt/src/Zuul_Framework/Start.java
+++ Gruppe 6 Semester Projekt/src/Zuul_Framework/Start.java
@@ -16,7 +16,7 @@ public class Start {
             Game game1 = new Game();
             game1.play();
         } catch (IOException e) {       // this is the catch where if an error from reading from the file shuld occur we want an errormessage printed
-                System.err.println("Caught IOException: " + e.getMessage());
+                System.err.println("Caught IOException: " + e.getMessage());    // the printline for the error messeage
 } 
     }
     
